5ea2379a1f7a697841044d5da61585d09a42fced
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-04-01 23:07:01 -0400

    Update AddLocalUser.ps1

--- AddLocalUser.ps1
+++ AddLocalUser.ps1
@@ -1,1 +1,6 @@
-
+New-LocalUser -Name "user_name" `
+-FullName "first last name" `
+-Description "Administrator of this Computer" `
+-Password (ConvertTo-SecureString -AsPlainText "P@ssw0rd01" -Force) `
+-PasswordNeverExpires `
+-AccountNeverExpires 
