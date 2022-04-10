8799caddacbf983f0f20b6caa8e24166afe380a7
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-04-09 23:23:24 -0400

    Update ConfigureSSH_server.ps1

--- ConfigureSSH_server.ps1
+++ ConfigureSSH_server.ps1
@@ -1,1 +1,11 @@
 
+Get-WindowsCapability -Online | ? Name -like 'OpenSSH*' 
+
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+Start-Service -Name "sshd" 
+
+Set-Service -Name "sshd" -StartupType Automatic 
+
+Get-Service -Name "sshd" | Select-Object *
+
